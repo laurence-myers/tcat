20c2d9a446534dad1c69f02215d9f267873e1bec
Laurence Dougal Myers <laurencedougalmyers@gmail.com>
2017-08-01 21:09:40 +1000

    Add some more basic directives

--- src/directives.ts
+++ src/directives.ts
@@ -20,17 +20,90 @@ export interface DirectiveData {
     parser : AttributeParser;
 }
 
+/*
+TODO:
+ form - parse "name" as an assignment to the scope.
+ input - name (property of the form), ng-model, ng-required, ng-minlength, ng-maxlength, ng-pattern, ng-change, ng-trim
+ input[checkbox] - ng-model, ng-true-value, ng-false-value, ng-change
+ input[date] - min (interp), max (interp), ng-min, ng-max, ng-required, ng-change
+ input[datetime-local]
+ input[email]
+ input[month]
+ input[number]
+ input[radio]
+ input[range]
+ input[text]
+ input[time]
+ input[url]
+ input[week]
+ ngController
+ ngForm
+ ngHide (multiElement)
+ ngIf (multiElement)
+ ngInclude
+ ngNonBindable - don't parse children
+ ngOptions
+ ngPluralize
+ ngRepeat (multiElement)
+ ngShow (multiElement)
+ script
+ select
+ textarea
+
+
+ Expose $event on all event directives
+ */
+
 const BUILTIN_SINGLE_ATTRIBUTE_DIRECTIVE_NAMES = [
+    'ng-bind',
+    'ng-bind-html',
+    'ng-bind-template',
+    'ng-blur',
+    'ng-change',
+    'ng-checked',
     'ng-class',
+    'ng-class-even',
+    'ng-class-odd',
     'ng-click',
+    'ng-cloak',
+    'ng-copy',
+    'ng-cut',
+    'ng-dblclick',
+    'ng-disabled',
+    'ng-focus',
     'ng-hide',
     'ng-if',
-    'ng-show'
+    'ng-init',
+    'ng-keydown',
+    'ng-keypress',
+    'ng-keyup',
+    'ng-maxlength',
+    'ng-minlength',
+    'ng-model',
+    'ng-model-options',
+    'ng-mousedown',
+    'ng-mouseenter',
+    'ng-mouseleave',
+    'ng-mousemove',
+    'ng-mouseover',
+    'ng-mouseup',
+    'ng-open',
+    'ng-paste',
+    'ng-pattern',
+    'ng-readonly',
+    'ng-required',
+    'ng-selected',
+    'ng-show',
+    'ng-style',
+    'ng-submit',
+    'ng-switch', // This has ng-switch-where in child nodes
+    'ng-value'
 ];
 
 const BUILTIN_SINGLE_ATTRIBUTE_INTERPOLATED_DIRECTIVE_NAMES = [
     'ng-href',
-    'ng-src'
+    'ng-src',
+    'ng-srcset'
 ];
 
 export const directiveMap : Map<string, DirectiveData> = new Map<string, DirectiveData>();
